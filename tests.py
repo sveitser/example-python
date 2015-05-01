4d8dcb055ccd3e71041e04c13184dbc005a06edb
Mathis Antony <sveitser@gmail.com>
2015-05-01 17:21:42 +0800

    blah

--- tests.py
+++ tests.py
@@ -7,6 +7,10 @@ class TestMethods(unittest.TestCase):
     def test_add(self):
         self.assertEqual(awesome.smile(), ":)")
 
+class TestAgain(unittest.TestCase):
+    def test_some(self):
+        self.assertEqual(awesome.frown(), ":(")
+
 
 if __name__ == '__main__':
     unittest.main()
